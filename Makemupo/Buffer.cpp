df9ac30a66b4631508c6b3e84fd2b1dd7da06071
KevinRobichon <kevin.robichon@gmail.com>
2014-09-28 14:19:36 +0200

    buffer methods

--- Makemupo/Buffer.cpp
+++ Makemupo/Buffer.cpp
@@ -12,4 +12,20 @@ namespace MKGraphics {
 		glDeleteBuffers(1, &_id);
 	}
 
+	void Buffer::allocate(size_t size, Usage usage) {
+		Target target = Target::ArrayBuffer;
+		bind(target);
+		glBufferData((GLenum)target, size, NULL, (GLenum)usage);
+	}
+
+	void Buffer::fill(size_t offset, size_t size, void *data) {
+		Target target = Target::ArrayBuffer;
+		bind(target);
+		glBufferSubData((GLenum)target, offset, size, data);
+	}
+
+	void Buffer::bind(Target target) {
+		glBindBuffer((GLenum)target, _id);
+	}
+
 }
--- Makemupo/Buffer.h
+++ Makemupo/Buffer.h
@@ -8,8 +8,28 @@ namespace MKGraphics {
 	class Buffer: public Object
 	{
 	public:
+		enum class Usage : GLenum {
+			StreamDraw = GL_STREAM_DRAW,
+			StreamRead = GL_STREAM_READ,
+			StreamCopy = GL_STREAM_COPY,
+			StaticDraw = GL_STATIC_DRAW,
+			StaticRead = GL_STATIC_READ,
+			StaticCopy = GL_STATIC_COPY,
+			DynamicDraw = GL_DYNAMIC_DRAW,
+			DynamicRead = GL_DYNAMIC_READ,
+			DynamicCopy = GL_DYNAMIC_COPY
+		};
+
+		enum class Target : GLenum {
+			ArrayBuffer = GL_ARRAY_BUFFER
+		};
+
 		Buffer();
 		~Buffer();
+
+		void allocate(size_t size, Usage usage);
+		void fill(size_t offset, size_t size, void *data);
+		void bind(Target target);
 	};
 
 }
