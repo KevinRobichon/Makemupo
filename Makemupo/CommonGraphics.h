0f413777c6680b691963bbff8b52262eda699293
KevinRobichon <kevin.robichon@gmail.com>
2014-09-28 13:34:31 +0200

    little changes

--- Makemupo/CommonGraphics.h
+++ Makemupo/CommonGraphics.h
@@ -1,3 +1,18 @@
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+namespace MKGraphics {
+
+	enum class Type : GLenum {
+		Float = GL_FLOAT,
+		Int = GL_INT,
+		UnsignedByte = GL_UNSIGNED_BYTE
+	};
+
+	enum class Bool : GLenum {
+		True = GL_TRUE,
+		False = GL_FALSE
+	};
+
+}
